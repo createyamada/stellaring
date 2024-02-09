df0003062289ac209613358d09adf5196b484c29
sugita177 <moriaki.sugita.1730@gmail.com>
2024-02-09 16:39:48 +0900

    json送受信テスト作成

--- src/app/api/routes/test_router.py
+++ src/app/api/routes/test_router.py
@@ -1,11 +1,21 @@
 from fastapi import APIRouter
 from pydantic import BaseModel, Field
+import time
+import random
 
 router = APIRouter()
 
 
 class StateScheme(BaseModel):
     test_num: float
+
+
+@router.get("/test")
+def test_get():
+    sleep_time = random.uniform(1, 5)
+    time.sleep(sleep_time)
+    return {"test_num": "success!",
+            "sleep_time": sleep_time}
 
 
 @router.post("/test")
