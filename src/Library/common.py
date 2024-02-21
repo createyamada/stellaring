650702c06783de4768b494d2d5589b4903750931
createyamada <yamadayeah90@gmail.com>
2024-02-21 21:01:28 +0900

    共通処理に移動

--- src/Library/common.py
+++ src/Library/common.py
@@ -1,25 +1,90 @@
-import sympy
-sympy.init_printing(use_unicode=True)
+import numpy as np
 
-# 微分する関数
-# @param eq 式
-# @param int cnt 微分回数
-# @return sympy.core.add.Add
-def differential(eq , cnt:int):
-    x = sympy.Symbol('g')
-    print(sympy.diff(eq, x, cnt))
-    return sympy.diff(eq, x, cnt)
 
-# 積分する関数
-# @param eq 式
-# @param int cnt 積分回数
-# @return sympy.core.add.Add 
-def integral(eq , cnt:int):
-    x = sympy.Symbol('x')
-    eq = 2 * x ** 2 + 5 * x - 3
-    print(type(eq))
-    for num in range(cnt):
-        eq = sympy.integrate(eq)
-        print(eq)
-    return eq
+def numerical_solution_euler(f:object, vx:float, vy:float, dt:np.ndarray):
+    """
+    オイラー法による常微分方程式の数値解法
+
+    Parameters:
+    - f:object 速度を更新する力{"x":vxの力,"y":vyの力}
+    - vx: x軸初速度
+    - vy: y軸初速度
+    - dt:array 刻み幅リスト
+
+    Returns:
+    - result: x,y軸の位置ベクトルのリスト[{"x":0,"y":0},...]
+    """
+    # 初期条件
+    # 刻み幅
+    h = dt[1] - dt[0]
+    # 結果格納用配列
+    result = [0] * len(dt)
+    # 初期位置
+    result[0] = {
+        "x":0,
+        "y":0
+    }
+
+    # オイラー法による更新
+    for i in range(len(dt) - 1):
+        result[i+1] = {
+            "x":result[i]["x"] + vx,
+            "y":result[i]["y"] + (vy * h)
+        }
+        # 速度を更新する
+        vx -= f["x"] * h
+        vy -= f["y"] * h
+        if (result[i+1]["y"] < 0):
+            break
+    return result
+
+
+
+def numerical_solution_rungekutta(f:object, vx:float, vy:float, dt:np.ndarray):
+    """
+    ルンゲクッタ法による常微分方程式の数値解法
+
+    Parameters:
+    - f:object 速度を更新する力{"x":vxの力,"y":vyの力}
+    - vx: x軸初速度
+    - vy: y軸初速度
+    - dt:array 刻み幅リスト
+
+    Returns:
+    - result: x,y軸の位置ベクトルのリスト[{"x":0,"y":0},...]
+    """
+    # 初期条件
+    # 刻み幅
+    h = dt[1] - dt[0]
+    # 結果格納用配列
+    result = [0] * len(dt)
+    # 初期位置
+    result[0] = {
+        "x":0,
+        "y":0
+    }
     
+
+    # ルンゲ-クッタ法による更新
+    for i in range(len(dt) - 1):
+        k1_x = vx
+        k2_x = vx + h / 2 * k1_x
+        k3_x = vx + h / 2 * k2_x 
+        k4_x = vx + h * k3_x
+
+        k1_y = vy
+        k2_y = vy + h / 2 * k1_y
+        k3_y = vy + h / 2 * k2_y 
+        k4_y = vy + h * k3_y
+
+        result[i+1] = {
+            "x":result[i]["x"] + (k1_x + 2 * k2_x + 2 * k3_x + k4_x) / 6,
+            "y":result[i]["y"] + (k1_y + 2 * k2_y + 2 * k3_y + k4_y) / 6
+        }
+
+        # 速度を更新する
+        vx -= f["x"]
+        vy -= f["y"] * h
+        if (result[i]["y"] < 0):
+            break
+    return result
--- src/app/api/routes/equationOfMotion.py
+++ src/app/api/routes/equationOfMotion.py
@@ -1,23 +1,21 @@
 from fastapi import APIRouter
 from service import equationOfMotion
 from library import common
-import sympy
 import numpy as np
-from scipy.integrate import odeint
 from library import const
+from pydantic import BaseModel
 
 router = APIRouter()
 
-@router.get('/')
-# def index(speed: int , angle: float , type: int):
-def index():
+class StateScheme(BaseModel):
+    speed: float
+    angle: float
+    step: float
+    type: int
 
+@router.get('/')
+def index(speed: float , angle: float , step:float , type: int):
     # 初期条件
-    speed = 100
-    angle = 45.0
-    position = 0
-    step = 0.1
-    type = 1
     num_steps = 100
     theta_rad = np.radians(angle)
     vx = speed * np.cos(theta_rad)
@@ -29,17 +27,17 @@ def index():
     #  取得する配列（時間間隔）を作成
     dt = np.arange(0,num_steps,step) 
 
+    # print(type(dt))
     if type == 0:
         # 解析解の場合
         result = [0] * len(dt)
+        position = 0
         # 位置の解析解を計算
         for i in range(len(dt)-1):
-            print(i)
             result[i] = {
                 "x":vx * dt[i],
                 "y":position + vy * dt[i] - 0.5 * const.GRAVITATIONAL_ACCELERATION * dt[i]**2
             }
-            print(result[i])
             if (result[i]["y"] < 0):
                 break
 
@@ -48,138 +46,16 @@ def index():
         # 数値解の場合
         if type == 1:
             # オイラー法の場合
-            result = numerical_solution_euler(f, vx, vy, dt)
+            result = common.numerical_solution_euler(f, vx, vy, dt)
         elif type == 2:
             # ルンゲクッタ法の場合
-            result = numerical_solution_rungekutta(f, vx, vy, dt)
-
-
+            result = common.numerical_solution_rungekutta(f, vx, vy, dt)
 
 
     return result
 
 
-def numerical_solution_euler(f, vx, vy, dt):
-    """
-    オイラー法による常微分方程式の数値解法
-
-    Parameters:
-    - f:array 速度を更新する力[vxの力,vyの力]
-    - vx: x軸初速度
-    - vy: y軸初速度
-    - dt:array 刻み幅リスト
-
-    Returns:
-    - result: x,y軸の位置ベクトルのリスト[{"x":0,"y":0},...]
-    """
-    print(f)
-    print(f["x"])
-    # 初期条件
-    # 刻み幅
-    h = dt[1] - dt[0]
-    # 初期位置
-    # 結果格納用配列
-    result = [0] * len(dt)
-    result[0] = {
-        "x":0,
-        "y":0
-    }
 
 
 
-    # こいつは多分いらない
-    x = [0] * len(dt)
-    y = [0] * len(dt)
-    y[0] = 0
-    x[0] = 0
-
-    # オイラー法の計算
-    for i in range(len(dt) - 1):
-        x[i + 1] = x[i] + vx
-        y[i + 1] = y[i] + (vy * h)
-        result[i+1] = {
-            "x":x[i+1],
-            "y":y[i+1]
-        }
-        # 速度を更新する
-        vx = vx
-        vy -= const.GRAVITATIONAL_ACCELERATION * h
-        if (y[i+1] < 0):
-            break
-    return result
-
-
-def numerical_solution_rungekutta(f, vx, vy, dt):
-
-    """
-    ルンゲクッタ法による常微分方程式の数値解法
-
-    Parameters:
-    - f:array 速度を更新する力[vxの力,vyの力]
-    - vx: x軸初速度
-    - vy: y軸初速度
-    - dt:array 刻み幅リスト
-
-    Returns:
-    - result: x,y軸の位置ベクトルのリスト[{"x":0,"y":0},...]
-    """
-    # 初期条件
-    # 刻み幅
-    h = dt[1] - dt[0]
-    # 初期位置
-    # 結果格納用配列
-    result = [0] * len(dt)
-    result[0] = {
-        "x":0,
-        "y":0
-    }
-
-
-
-    # こいつは多分いらない
-    x = [0] * len(dt)
-    y = [0] * len(dt)
-    y[0] = 0
-    x[0] = 0
-    
-
-    # ルンゲ-クッタ法による更新
-    for i in range(len(dt) - 1):
-        k1_x = vx
-        k2_x = vx + h / 2 * k1_x
-        k3_x = vx + h / 2 * k2_x 
-        k4_x = vx + h * k3_x
-
-        k1_y = vy
-        k2_y = vy + h / 2 * k1_y
-        k3_y = vy + h / 2 * k2_y 
-        k4_y = vy + h * k3_y
-
-        x[i + 1] = x[i] + (k1_x + 2 * k2_x + 2 * k3_x + k4_x) / 6
-        y[i + 1] = y[i] + (k1_y + 2 * k2_y + 2 * k3_y + k4_y) / 6
-        result[i+1] = {
-            "x":x[i + 1],
-            "y":y[i + 1]
-        }
-
-        # 速度を更新する
-        vx = vx
-        vy -= const.GRAVITATIONAL_ACCELERATION * h
-        if (y[i+1] < 0 ):
-            break
-    return result
-
-
-def test():
-    """
-    放物運動力
-
-    Parameters:
-    - None
-
-    Returns:
-    - return:　力[x軸にかかる力,y軸にかかる力] 
-    """
-
-    return {"x":0,"y":const.GRAVITATIONAL_ACCELERATION}
 
